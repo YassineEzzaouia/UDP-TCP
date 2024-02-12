3a1636b4b6ae06d53a4226c84d44d70d2708fcc0
YassineEzzaouia <109194591+YassineEzzaouia@users.noreply.github.com>
2024-02-12 18:49:42 +0100

    Update UDPClient.java

--- UDPClient.java
+++ UDPClient.java
@@ -3,18 +3,18 @@ import java.net.*;
 public class UDPClient {
     public static void main(String[] args) {
         try {
-            // Créer une socket pour le client
+           
             DatagramSocket clientSocket = new DatagramSocket();
             InetAddress IPAddress = InetAddress.getByName("localhost");
             byte[] sendData;
             String sentence = "Hello, serveur UDP!";
             sendData = sentence.getBytes();
 
-            // Préparer le paquet pour envoyer les données au serveur
+     
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-            // Envoyer les données au serveur
+        
             clientSocket.send(sendPacket);
-            // Fermer la socket client
+          
             clientSocket.close();
         } catch (Exception e) {
             e.printStackTrace();
